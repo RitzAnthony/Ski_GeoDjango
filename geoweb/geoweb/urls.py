39de8470aa3589879803a60546987e8d9ef0de07
Anthony <anthony.ritz@gmx.ch>
2019-04-27 12:17:57 +0200

    unstable version on home page

--- geoweb/geoweb/urls.py
+++ geoweb/geoweb/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', include('swissgeo.urls')),
+    path('/', include('home.urls')),
     path('swissgeo/', include('swissgeo.urls')),
     path('admin/', admin.site.urls),
     path('skiresorts/', include('skiresorts.urls'))
